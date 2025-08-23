6a8492e0f5a5b7fb82cc5b84962507af0e948ec2
Bhargavi <bhargavi.k@quanbytech.onmicrosoft.com>
2025-08-23 16:27:49 +0530

    arithmetic Operators

---
+++ arithmeticOperators.js
@@ -0,0 +1,31 @@
+function addNumbers(a, b) {
+    a = Number(a);
+    b = Number(b);
+    document.getElementById("lblResult").innerHTML = a + b;
+
+}
+
+function subNumbers(a, b) {
+    a = Number(a);
+    b = Number(b);
+    document.getElementById("lblResult").innerHTML = a - b;
+
+}
+function mulNumbers(a, b) {
+    a = Number(a);
+    b = Number(b);
+    document.getElementById("lblResult").innerHTML = a * b;
+
+}
+function divNumbers(a, b) {
+    a = Number(a);
+    b = Number(b);
+    document.getElementById("lblResult").innerHTML = a / b;
+
+}
+function modNumbers(a, b) {
+    a = Number(a);
+    b = Number(b);
+    document.getElementById("lblResult").innerHTML = a % b;
+
+}
