1902294444118b8f2437787402d403623a1aa072
Bhargavi <bhargavi.k@quanbytech.onmicrosoft.com>
2025-08-24 11:11:01 +0530

    Objects html and Objects.js

---
+++ objects.js
@@ -0,0 +1,7 @@
+const  homeProperties = { name: "Vedam Mahal", address: "Kathef Street", city: "Eluru", state: "AP" };
+
+
+document.getElementById("pName").innerHTML = homeProperties.name;
+document.getElementById("pAddress").innerHTML = homeProperties.address;
+document.getElementById("pCity").innerHTML = homeProperties.city;
+document.getElementById("pState").innerHTML = homeProperties.state;
